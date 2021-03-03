9ff83e99a82c4f61dc87c55a10af760312b1ef03
kienzhe <lekim@live.dk>
2021-03-03 11:20:57 +0100

    Update search.js

--- Projekt/Klippekortet/js/search.js
+++ Projekt/Klippekortet/js/search.js
@@ -3,7 +3,20 @@ console.log('koden er inde');
 function validateForm() {
   var x = document.forms["searchForm"]["fname"].value;
   if (x == "") {
-    alert("Du skal indtaste et  postnummer for at forsætte");
+    alert("Du skal indtaste et postnummer for at forsætte");
     return false;
   }
 }
+
+const citys = ['5100', '5250', '8100', '7100', '2600', '2625', '2200', '2400' ];
+
+document.getElementById('fname').addEventListener('input', (e)=>{
+
+  let citysArray = [];
+
+  console.log(e.target.value){
+    citysArray = citys.filter(city => city.toLowerCase().includes(e.target.value));
+  }
+
+  console.log(citysArray);
+})
