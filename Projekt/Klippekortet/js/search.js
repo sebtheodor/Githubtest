c3fc12aca6c71d21a65c3cae0abc08046a22c7a3
kienzhe <lekim@live.dk>
2021-03-03 10:04:36 +0100

    Delete search.js

--- Projekt/Klippekortet/js/search.js
+++
@@ -1,9 +0,0 @@
-console.log('koden er inde');
-
-function validateForm() {
-  var x = document.forms["searchForm"]["fname"].value;
-  if (x == "") {
-    alert("Du skal indtaste et  postnummer for at forsætte");
-    return false;
-  }
-}
