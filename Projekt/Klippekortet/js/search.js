a6aca1acd08ad689ec67ce8f4eb0d8624953cebd
kienzhe <lekim@live.dk>
2021-03-03 11:30:44 +0100

    Update search.js

--- Projekt/Klippekortet/js/search.js
+++ Projekt/Klippekortet/js/search.js
@@ -20,3 +20,23 @@ document.getElementById('fname').addEventListener('input', (e)=>{
 
   console.log(citysArray);
 })
+
+
+const citys = ['5100', '5250', '8100', '7100', '2600', '2625', '2200', '2400' ];
+
+document.getElementById('fname').addEventListener('input', (e)=>{
+
+  let citysArray = [];
+
+  if(e.target.value){
+    citysArray = citys.filter(city => city.toLowerCase().includes(e.target.value));
+    citysArray = citysArray.map(city => `<li>${city}</li>`)
+  }
+
+  showCitysArray(citysArray);
+});
+
+function showCitysArray(citysArray){
+  const html = !citysArray.length ? '' : citysArray.join('');
+  document.querySelector('ul').innerHTML = html;
+}
