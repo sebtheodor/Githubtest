568bf4f0242c2b2a9f3d8d080905f600e15838ff
kienzhe <lekim@live.dk>
2021-03-04 16:59:04 +0100

    Update addrese.js

--- Projekt/Klippekortet/js/addrese.js
+++ Projekt/Klippekortet/js/addrese.js
@@ -8,7 +8,7 @@ function validateForm() {
   }
 }
 
-//autocomplete
+//
 const citys = ['5100', '5250', '8100', '7100', '2600', '2625', '2200', '2400' ];
 
 document.getElementById('fname').addEventListener('input', (e)=>{
