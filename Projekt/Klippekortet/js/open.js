e457713d4790326de94526d60a0b307ebb018beb
NickuWasTaken <74908215+NickuWasTaken@users.noreply.github.com>
2021-03-04 16:54:34 +0100

    +forside, +login
    edit: open.js
    
    +billeder

--- Projekt/Klippekortet/js/open.js
+++ Projekt/Klippekortet/js/open.js
@@ -2,7 +2,7 @@
 // Mindre script der checker hvad klokken er på et givet tidspunkt
 // Efter dette oprettes der et array udfra content der har klassen "open" 
 // Contenten bliver herefter skiftet ud med baseret på udfoldelelsen af for løkken
-// der bestemmer om klokken er mere end 9 og tidligere end 17 
+// bestemmes det om klokken er mere end 9 og tidligere end 17 
 // (Fremvises på search.html)
 //															- Nicklas
 
